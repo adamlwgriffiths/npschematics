23566f097cef2f2c91971bdfe4d5e30ed27a3edc
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-09-20 14:35:14 +1000

    Initial commit.
    Taken NumpyType from personal schematics fork
    (https://github.com/adamlwgriffiths/schematics)
    and moved into its own repo as per suggestions.
    Renamed NumpyType to NumpyArrayType.
    Fixed serialisation.
    Added serialisation tests.

--- README.md
+++ README.md
@@ -1,4 +1,87 @@
-npschematics
+============
+NpSchematics
 ============
 
-Numpy types for Schematics
+.. rubric:: Numpy types for Schematics.
+
+.. image:: https://secure.travis-ci.org/adamlwgriffiths/npschematics.png?branch=master
+  :target: https://secure.travis-ci.org/adamlwgriffiths/npschematics
+  :alt: Build Status
+
+
+About
+=====
+
+Adds Numpy Types to the Schematics module.
+
+Schematics is a Python library to combine types into structures, validate them,
+and transform the shapes of your data based on simple descriptions.
+
+The internals are similar to ORM type systems, but there is no database layer
+in Schematics.  Instead, Schematics believes the task of building a database
+layer is made significantly easier when Schematics handles everything but
+writing the query.
+
+Further, it can be used for a range of tasks where having a database involved
+may not make sense.
+
+Some common use cases:
+
++ Enforce array dtype, size, shape, dimensions.
++ Serialise / deserialise models
+
+
+Examples
+--------
+
+This is a simple Model with Schamtics and NpSchematics.
+
+::
+
+  >>> from schematics.models import Model
+  >>> from schematics.types import StringType, URLType
+  >>> from npschematics.types import NumpyArrayType
+  >>> class Person(Model):
+  ...     name = StringType(required=True)
+  ...     website = URLType()
+  ...     ratings = NumpyArrayType(required=True, dtype='int16', shape=(5,), default=np.array([0]*5))
+  ...
+  >>> person = Person({'name': u'Joe Strummer', 
+  ...                  'website': 'http://soundcloud.com/joestrummer'})
+  >>> person.name
+  u'Joe Strummer'
+  >>> person.ratings
+  [0, 0, 0, 0, 0]
+
+Serializing the data to JSON.
+
+::
+
+  >>> import json
+  >>> json.dumps(person.to_primitive())
+  {"name": "Joe Strummer", "website": "http://soundcloud.com/joestrummer", "ratings": "[0, 0, 0, 0, 0]"}
+
+Let's try validating with an invalid shape.
+
+::
+
+  >>> person = Person()
+  >>> person.ratings = [1,2,3]
+  >>> person.validate()
+  Traceback (most recent call last):
+    File "<stdin>", line 1, in <module>
+    File "schematics/models.py", line 231, in validate
+      raise ModelConversionError(e.messages)
+  schematics.exceptions.ModelConversionError: {'ratings': [u'Could not convert shape']}
+
+
+Add the field and validation passes
+
+::
+
+  >>> person = Person()
+  >>> person.name = 'Amon Tobin'
+  >>> person.website = 'http://www.amontobin.com/'
+  >>> person.ratings = [1,2,3,4,5]
+  >>> person.validate()
+  >>> 
---
+++ tests/test_numpy_type.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+import unittest
+
+from schematics.models import Model
+from schematics.contrib.numpy import NumpyArrayType
+from schematics.exceptions import ValidationError, ConversionError
+
+import numpy as np
+
+class TestNumpyArrayType(unittest.TestCase):
+    def test_numpy_type_with_model_with_numpy_array(self):
+        class TestModel(Model):
+            data = NumpyArrayType()
+
+        m = TestModel()
+        m.data = np.array([1,2,3,4])
+        m.validate()
+
+        self.assertTrue(isinstance(m.data, np.ndarray))
+        self.assertTrue(np.array_equal(m.data, [1,2,3,4]))
+
+    def test_numpy_type_with_model_with_list(self):
+        class TestModel(Model):
+            data = NumpyArrayType()
+
+        m = TestModel()
+        m.data = [1,2,3,4]
+        m.validate()
+
+        self.assertTrue(isinstance(m.data, np.ndarray))
+        self.assertTrue(np.array_equal(m.data, [1,2,3,4]))
+
+    def test_numpy_type_with_model_with_tuple(self):
+        class TestModel(Model):
+            data = NumpyArrayType()
+
+        m = TestModel()
+        m.data = (1,2,3,4)
+        m.validate()
+
+        self.assertTrue(isinstance(m.data, np.ndarray))
+        self.assertTrue(np.array_equal(m.data, [1,2,3,4]))
+
+    def test_numpy_conversion_with_string(self):
+        with self.assertRaises(ConversionError):
+            NumpyArrayType()('abc')
+
+    def test_numpy_conversion_with_list(self):
+        NumpyArrayType()([1,2,3])
+
+    def test_numpy_conversion_with_tuple(self):
+        NumpyArrayType()(((1,2,3,),(4,5,6,)))
+
+    def test_numpy_conversion_with_list_of_tuples(self):
+        NumpyArrayType()([(1,2,3,),(4,5,6,)])
+
+    def test_numpy_type_with_numpy_array(self):
+        NumpyArrayType().validate(np.array([1,2,3,4,5]))
+
+    def test_numpy_type_with_dtype(self):
+        with self.assertRaises(ValidationError):
+            NumpyArrayType(dtype='float32').validate(np.array([1,2,3,4,5]))
+
+    def test_numpy_type_with_different_dtype(self):
+        with self.assertRaises(ValidationError):
+            NumpyArrayType(dtype='int64').validate(np.array([1,2,3,4,5], dtype='float32'))
+
+    def test_numpy_type_with_shape(self):
+        NumpyArrayType(shape=(5,)).validate(np.array([1,2,3,4,5]))
+
+    def test_numpy_type_with_invalid_shape(self):
+        with self.assertRaises(ValidationError):
+            NumpyArrayType(shape=(2,2,)).validate(np.array([1,2,3,4,5]))
+
+    def test_numpy_type_with_size(self):
+        NumpyArrayType(size=5).validate(np.array([1,2,3,4,5]))
+
+    def test_numpy_type_with_invalid_size(self):
+        with self.assertRaises(ValidationError):
+            NumpyArrayType(size=6).validate(np.array([1,2,3,4,5]))
+
+    def test_numpy_type_with_ndim(self):
+        NumpyArrayType(ndim=2).validate(np.array([[1,2],[3,4],[5,6]]))
+
+    def test_numpy_type_with_invalid_ndim(self):
+        with self.assertRaises(ValidationError):
+            NumpyArrayType(ndim=2).validate(np.array([1,2,3,4,5]))
+
+    def test_numpy_type_with_string(self):
+        with self.assertRaises(ValidationError):
+            NumpyArrayType(ndim=2).validate('abc')
+
+    def test_numpy_type_serialise(self):
+        class TestModel(Model):
+            i = NumpyArrayType(dtype='int32')
+            f = NumpyArrayType(dtype='float32')
+
+        m = TestModel({'i':[1,2,3,4,5], 'f':[1.,2.,3.,4.,5.]})
+        d = m.serialize()
+        m = TestModel(d)
+
+        self.assertTrue(isinstance(m.i, np.ndarray))
+        self.assertTrue(isinstance(m.f, np.ndarray))
+        self.assertTrue(np.array_equal(m.i, [1,2,3,4,5]))
+        self.assertTrue(np.array_equal(m.f, [1.,2.,3.,4.,5.]))
+
+    def test_numpy_type_json(self):
+        # commented out until 'Model.to_primitive' addition is pushed to pypi
+        """
+        class TestModel(Model):
+            i = NumpyArrayType(dtype='int32')
+            f = NumpyArrayType(dtype='float32')
+
+        import json
+        m = TestModel({'i':[1,2,3,4,5], 'f':[1.,2.,3.,4.,5.]})
+        d = json.dumps(m.to_primitive())
+        d = json.loads(d)
+        m = TestModel(d)
+
+        self.assertTrue(isinstance(m.i, np.ndarray))
+        self.assertTrue(isinstance(m.f, np.ndarray))
+        self.assertTrue(np.array_equal(m.i, [1,2,3,4,5]))
+        self.assertTrue(np.array_equal(m.f, [1.,2.,3.,4.,5.]))
+        """
+        pass
